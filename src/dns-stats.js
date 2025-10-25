9c479d72fc23966692d3c4971908741c64ac15ac
Vladislav <vlad.kvan96@gmail.com>
2025-10-26 00:20:24 +0300

    complete dns stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,43 @@ const { NotImplementedError } = require('../lib');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+
+function getDNSStats(domains) {
+  if (domains.length < 1) {
+    return {};
+  }
+  const dnsArr = domains.length > 1 ? [[domains[0].split('.')[domains.length - 1]]] : [[domains[0].split('.')[domains.length]]];
+  const result = {};
+  const sortArr = [];
+  let sortResult = {};
+  
+  function countMatch(arr, match) {
+    let count = 0;
+    for (let i = 1; i < arr.length; i += 1) {
+      if (arr[i].includes(match[match.length - 1])) {
+        count += 1;
+      }
+    }
+    return count;
+  }
+
+  for (let i = 0; i < domains.length; i += 1) {
+    dnsArr.push(domains[i].split('.').reverse());
+  }
+
+  for (let i = 0; i < dnsArr.length; i += 1) {
+    result['.' + dnsArr[i].join('.')] = countMatch(dnsArr, dnsArr[i]);
+  }
+  
+  for (let val in result) {
+    sortArr.push([val, result[val]]);
+  }
+
+  sortArr.sort((a, b) => b[1] - a[1]);
+
+  sortResult = Object.fromEntries(sortArr);
+
+  return sortResult;
 }
 
 module.exports = {
