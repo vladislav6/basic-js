c89273cc1ec1650bd9c4466d634da92694aac006
Vladislav <vlad.kvan96@gmail.com>
2025-10-31 23:26:51 +0200

    complete mac address and matrix elements sum

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,8 @@ const { NotImplementedError } = require('../lib');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function isMAC48Address(n) {
+  return /^([0-9A-Fa-f]{2}-){5}([0-9A-Fa-f]{2})$/.test(n);
 }
 
 module.exports = {
--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,9 +16,27 @@ const { NotImplementedError } = require('../lib');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+
+function getMatrixElementsSum(matrix) {
+  
+  let result = 0;
+
+  for (let i = 0; i < matrix[0].length; i++) {
+    result = result + matrix[0][i];
+  }
+
+  for (let i = 1; i <= matrix.length; i += 1) {
+    matrix[i - 1].map((val, ind) => {
+      if (matrix[i-1][ind] !== 0) {
+          if (i !== matrix.length) {
+            console.log(matrix[i][ind])
+            result = result + matrix[i][ind];
+          }
+        }
+    });
+  }
+
+  return result;
 }
 
 module.exports = {
